d6a700bc036a3e412557c41b9d3789c91dfc4f8e
nokranger <anusorn.thavornporn@gmail.com>
2020-01-20 17:03:06 +0700

    edit map

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -13,7 +13,7 @@
                     <b-col cols="12" sm="12" md="12" lg="6" xl="6">
                       <div>
                         <div class="card">
-                          <img v-b-modal.modal-lgc1 src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="...">
+                          <img v-b-modal.modal-lgc1 src="https://i.imgur.com/pdfcCXr.png" class="card-img-top" alt="...">
                           <br>
                           <div class="card-body">
                             <h5 class="card-title"><i class="fa fa-location-arrow" aria-hidden="true"></i> {{titleOA}}</h5>
@@ -25,17 +25,17 @@
                           </div>
                         </div>
                         <b-modal hide-footer id="modal-lgc1" size="lg">
-                          <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="..."></a>
+                          <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/pdfcCXr.png" class="card-img-top" alt="..."></a>
                         </b-modal>
                       </div>
                     </b-col>
                     <b-col cols="12" sm="12" md="12" lg="6" xl="6">
                       <div>
                         <div class="card">
-                          <img v-b-modal.modal-lgc2 src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="...">
+                          <img v-b-modal.modal-lgc2 src="https://i.imgur.com/Mvqg7IP.png" class="card-img-top" alt="...">
                           <br>
                           <div class="card-body">
-                            <h5 class="card-title"><i class="fa fa-location-arrow" aria-hidden="true"></i> {{titleOO}}</h5>
+                            <h5 class="card-title" style="margin-top:3px;"><i class="fa fa-location-arrow" aria-hidden="true"></i> {{titleOO}}</h5>
                             <p class="card-text">{{detailOO}}</p><br><br>
                             <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
                             <div class="align-right">
@@ -44,7 +44,7 @@
                           </div>
                         </div>
                         <b-modal hide-footer id="modal-lgc2" size="lg">
-                          <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="..."></a>
+                          <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/Mvqg7IP.png" class="card-img-top" alt="..."></a>
                         </b-modal>
                       </div>
                     </b-col>
@@ -92,7 +92,7 @@ export default {
       titleOO: 'Operational Office',
       detailOA: '55/39/1 Mhutee 13 Navanakorn Phahoyothin Road, Klong Nueng, Klong Luang, Patumtanee Thailand 12120',
       detailOO: '19/125 Soi Sukhumvit 13, Klong Toey Nua, Wattana, Bangkok 10110, Thailand',
-      linkOA: '#',
+      linkOA: 'https://www.google.com/maps/place/%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B8%A9%E0%B8%B1%E0%B8%97%E0%B8%8A%E0%B8%B1%E0%B8%A2%E0%B9%82%E0%B8%81%E0%B8%A1%E0%B8%A5%E0%B8%98%E0%B8%B8%E0%B8%A3%E0%B8%81%E0%B8%B4%E0%B8%88%E0%B8%88%E0%B8%B3%E0%B8%81%E0%B8%B1%E0%B8%94+%E0%B8%95%E0%B8%B3%E0%B8%9A%E0%B8%A5+%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%AB%E0%B8%99%E0%B8%B6%E0%B9%88%E0%B8%87+%E0%B8%AD%E0%B8%B3%E0%B9%80%E0%B8%A0%E0%B8%AD%E0%B8%84%E0%B8%A5%E0%B8%AD%E0%B8%87%E0%B8%AB%E0%B8%A5%E0%B8%A7%E0%B8%87+%E0%B8%9B%E0%B8%97%E0%B8%B8%E0%B8%A1%E0%B8%98%E0%B8%B2%E0%B8%99%E0%B8%B5+12120/@14.122681,100.6073938,17z/data=!3m1!4b1!4m5!3m4!1s0x30e27f5d2303305b:0x133eb0b70c9597c4!8m2!3d14.1226758!4d100.6095825',
       linkOO: 'https://www.google.co.th/maps/place/LOGIPROTECH+(THAILAND)+CO.,LTD./@13.7428174,100.5554025,17z/data=!3m1!4b1!4m5!3m4!1s0x30e29f468d5f8f5b:0x2085bde87d87f06b!8m2!3d13.7428122!4d100.5575912?hl=th'
     }
   },
