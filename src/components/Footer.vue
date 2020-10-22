cf732a1999a13f941aed700cc03fe2bcb61ccd0b
nokranger <anusorn.thavornporn@gmail.com>
2020-10-22 10:51:39 +0700

    update address

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -32,17 +32,17 @@
                 <ul class="footers">
                   <li>Email : <a href="mailto:info@logiprotech-th.co.th">info@logiprotech-th.co.th</a></li>
                   <li>Tel. : <a href="tel:029090320">+66(0)2909-0320</a></li>
-                  <li>Operational Office : 19/125 Soi Sukhumvit 13, Klong Toey Nua, Wattana, Bangkok 10110, Thailand</li>
+                  <li>Operational Office : {{OperationalOffice}}</li>
                 </ul>
             </div>
           </b-col>
           <b-col cols="12" sm="12" md="12" lg="4" xl="4">
             <div>
               <div class="card">
-                <img v-b-modal.modal-lg src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="...">
+                <img v-b-modal.modal-lg src="https://i.imgur.com/TokcC60.png" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title"><i class="fa fa-location-arrow" aria-hidden="true"></i> LPTT</h5>
-                  <p class="card-text">19/125 Soi Sukhumvit 13, Klong Toey Nua, Wattana, Bangkok 10110, Thailand</p>
+                  <p class="card-text">{{OperationalOffice}}</p>
                   <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
                   <div class="align-right">
                     <button class="button"><a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:50px;height:50px;" src="https://i.imgur.com/JKjVDrz.png" alt=""></a></button>
@@ -50,7 +50,7 @@
                 </div>
               </div>
               <b-modal hide-footer id="modal-lg" size="lg">
-                <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/CdVCi8i.png" class="card-img-top" alt="..."></a>
+                <a class="linkmap" target="_blank" v-bind:href="linkOA"><img style="width:100%" src="https://i.imgur.com/TokcC60.png" class="card-img-top" alt="..."></a>
               </b-modal>
             </div>
           </b-col>
@@ -75,7 +75,8 @@
 export default {
   data () {
     return {
-      linkOA: 'https://www.google.co.th/maps/place/LOGIPROTECH+(THAILAND)+CO.,LTD./@13.7428174,100.5554025,17z/data=!3m1!4b1!4m5!3m4!1s0x30e29f468d5f8f5b:0x2085bde87d87f06b!8m2!3d13.7428122!4d100.5575912?hl=th'
+      linkOA: 'https://www.google.com/maps/place/%E0%B8%9A%E0%B8%88%E0%B8%81.+%E0%B8%A1%E0%B9%82%E0%B8%99%E0%B8%A2%E0%B8%99%E0%B8%95%E0%B9%8C%E0%B8%8A%E0%B8%B1%E0%B8%A2/@13.7449639,100.5121174,17.17z/data=!4m5!3m4!1s0x0:0x8e45fe8733d02469!8m2!3d13.7456285!4d100.514041',
+      OperationalOffice: '594 Luang Road, Pom Prab Sub-district, Pom Prap Sattru Phai District, Bangkok, 10100'
     }
   }
 
